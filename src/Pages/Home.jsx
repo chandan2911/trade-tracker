dbd121c6b24fd9fe984a8fc461177c822bd836db
Chandan <54211830+chandan9080@users.noreply.github.com>
2022-03-15 16:20:21 +0530

    Testing another api

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../Components/HeroSection/Hero";
 import MainSection from "../Components/mainSection/MainSection";
+import TestRank from "./TestRank";
 
 const Home = () => {
   return (
     <div>
-      <Hero></Hero>
-      <MainSection></MainSection>
+      <TestRank></TestRank>
+      {/* <Hero></Hero>
+      <MainSection></MainSection> */}
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import App from "./App";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
+
 render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
-      
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
