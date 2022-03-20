0a8519dadfefe5c62c5b350338debcae7cb731a3
Chandan <54211830+chandan9080@users.noreply.github.com>
2022-03-20 13:56:06 +0530

    dropdown making

--- client/src/Components/DropDown/currency/CurrencyDropDown.jsx
+++ client/src/Components/DropDown/currency/CurrencyDropDown.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { currencyArr } from "../../../ExpectedValue";
 import { setCurrency } from "../../../redux/currencySlice";
@@ -7,26 +7,40 @@ import "./CurrencyDropDown.css";
 const CurrencyDropdown = () => {
   const dispatch = useDispatch();
   const { sign } = useSelector((state) => state.currency);
-
+  const [toggle, setToggle] = useState(false);
   const handleChange = (currency) => {
     dispatch(setCurrency(currency));
   };
   return (
     <div className="CurrencyDropdown">
-      <button className="dropbtn">Currency:{sign} 🔽 </button>
-      <div className="dropdown-content">
-        {currencyArr.map((currency) => (
-          <div 
-            key={currency.uuid}
-            value={currency.name}
-            onClick={() => {
-              handleChange(currency);
-            }}
-          >
-            {currency.name}
-          </div  >
-        ))}
-      </div>
+      <button
+        onClick={() => {
+          setToggle(!toggle);
+        }}
+        className="dropbtn"
+      >
+        <span>Currency:{sign}</span>
+
+        <i class="fa fa-angle-down"></i>
+      </button>
+
+      {toggle && (
+        <ul className="dropdown-content">
+          {currencyArr.map((currency) => (
+            <li
+              key={currency.uuid}
+              value={currency.name}
+              onClick={() => {
+                handleChange(currency);
+                setToggle(!toggle);
+              }}
+              className="dropdown-item"
+            >
+              {currency.name}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
