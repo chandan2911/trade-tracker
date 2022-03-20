52f22671c94152cd074c7eb54e9ae1a922d2c6e8
Chandan <54211830+chandan9080@users.noreply.github.com>
2022-03-20 14:24:25 +0530

    orderby dropdown made

--- client/src/Components/DropDown/OrderBy.jsx
+++ client/src/Components/DropDown/OrderBy.jsx
@@ -1,23 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { orderByArr } from "../../ExpectedValue";
 import { setOrderBy } from "../../redux/orderBySlice";
 
 const OrderBy = () => {
   const dispatch = useDispatch();
+  const [toggle, setToggle] = useState(false);
+  const handleChange = (orderBy) => {
+    dispatch(setOrderBy(orderBy));
+  };
+
   return (
-    <div className="Dropdown">
-      <select
-        onChange={(e) => {
-          dispatch(setOrderBy(e.target.value));
+    <div className="OrderByDropdown no-select">
+      <div
+        onClick={() => {
+          setToggle(!toggle);
         }}
+        className="dropbtn"
       >
-        {orderByArr.map((orderby) => (
-          <option key={orderby} value={orderby}>
-            {orderby}
-          </option>
-        ))}
-      </select>
+        <span>Order-by</span>
+        {toggle ? (
+          <i class="fa fa-angle-up"></i>
+        ) : (
+          <i class="fa fa-angle-down"></i>
+        )}
+      </div>
+      {toggle && (
+        <ul className="dropdown-content ">
+          {orderByArr.map((orderby) => (
+            <li
+              key={orderby}
+              onClick={() => {
+                handleChange(orderby);
+                setToggle(!toggle);
+              }}
+              className="dropdown-item"
+            >
+              {orderby === "24hVolume change" ? "24h Volume" : orderby}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
