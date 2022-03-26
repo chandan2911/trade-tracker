33066759effa70652a7867cd714b3499336146d8
Chandan <54211830+chandan9080@users.noreply.github.com>
2022-03-26 14:26:01 +0530

    Design part pending

--- client/src/Components/SingleCoinDetails/SingleCoinDetails.jsx
+++ client/src/Components/SingleCoinDetails/SingleCoinDetails.jsx
@@ -11,6 +11,17 @@ const SingleCoinDetails = (props) => {
   const [Error, setError] = useState("");
   const [isLoading, setisLoading] = useState(false);
 
+  const MinimalNumber = (num) => {
+    if (num >= 1000000000) {
+      return (num / 1000000000).toFixed(2) + "B";
+    } else if (num >= 1000000) {
+      return (num / 1000000).toFixed(2) + "M";
+    } else if (num >= 1000) {
+      return (num / 1000).toFixed(2) + "K";
+    } else {
+      return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
+  };
   useEffect(async () => {
     setisLoading(true);
     await axios
@@ -32,30 +43,59 @@ const SingleCoinDetails = (props) => {
       });
   }, [time, currency]);
   console.log(Data);
+  const removeTags = (str) => {
+    return str.replace(/<\/?[^>]+(>|$)/g, "");
+  };
+  console.log(typeof Data);
   return (
     <div>
       {isLoading ? (
         <div>Loading...</div>
+      ) : Error.length > 0 ? (
+        <div>{Error}</div>
       ) : (
-        <div className="coin_details">
-          <header>
-            <img src={Data?.iconUrl} alt={Data?.name} width={30} />
-            <button
-              className="coin_details_header"
-              onClick={() => {
-                window.open(Data?.websiteUrl);
-              }}
-            >
-              {Data?.name}
-              <span className="coin_details_rank">{Data?.rank}</span>
-            </button>
-          </header>
-          <div className="coin_details_body">
-            <div className="price_detail_body">
-              {currency?.sign} {Data?.price}
+        Data && (
+          <div className="coin_details">
+            <header>
+              <img src={Data?.iconUrl} alt={Data?.name} width={30} />
+              <h1
+                className="coin_details_header"
+                onClick={() => {
+                  window.open(Data?.websiteUrl);
+                }}
+              >
+                {Data?.name}
+                <span className="coin_details_rank">{Data?.rank}</span>
+              </h1>
+            </header>
+            <div className="coin_details_body">
+              <div className="coin_detail_left">
+                <span className="coin_price">
+                  Price:
+                  <span>{Data?.price}</span>
+                </span>
+                <span className="coin_rank">
+                  Rank:
+                  <span>{Data?.rank}</span>
+                </span>
+                <span className="coin_change">
+                  Change:
+                  <span>{Data?.change}</span>
+                </span>
+                <span className="coin_market_cap">
+                  Market Cap:
+                  <span>{Data?.marketCap}</span>
+                </span>
+              </div>
+              <div className="coin_detail_right">
+                <span className="coin_bio">
+                  Bio:
+                  <span>{Data?.description}</span>
+                </span>
+              </div>
             </div>
           </div>
-        </div>
+        )
       )}
     </div>
   );
