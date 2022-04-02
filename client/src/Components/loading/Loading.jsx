70d740d9cb918dd1819f62edb8b7e0578e42bb18
Chandan <54211830+chandan9080@users.noreply.github.com>
2022-04-02 12:36:29 +0530

    Loader added

--- client/src/Components/loading/Loading.jsx
+++ client/src/Components/loading/Loading.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Lottie from "react-lottie";
+import animationData from "../../lotties/loader.json";
 
 const Loading = () => {
-  return <div>Hello</div>;
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+  return (
+    <div>
+      <Lottie options={defaultOptions} height={400} width={400} />
+    </div>
+  );
 };
 
 export default Loading;
--- client/src/Components/mainSection/MainSection.jsx
+++ client/src/Components/mainSection/MainSection.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import OrderByDirection from "../DropDown/OrderByDirection";
+
 import Loading from "../loading/Loading";
 import Table from "../Showtable/table";
 
